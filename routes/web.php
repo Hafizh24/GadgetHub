472ee870dd314838035e0c4ce7aef864fda1b05a
Hafizh24 <hafizhcbs@gmail.com>
2024-09-03 17:32:09 +0700

    add register & login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
 use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',HomePage::class);
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
