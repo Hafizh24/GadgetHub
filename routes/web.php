5c0146e77e6e281827efe5af8c14125380c06749
Hafizh24 <hafizhcbs@gmail.com>
2024-09-21 18:32:42 +0700

    feat: add product detail page

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@ use App\Livewire\Auth\LoginPage;
 use App\Livewire\Auth\RegisterPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',HomePage::class);
+Route::get('/', HomePage::class);
 Route::get('/products', ProductsPage::class);
+Route::get('/products/{slug}', ProductDetailPage::class);
 Route::get('/categories', CategoriesPage::class);
+
 
 
 Route::get('/login', LoginPage::class);
