712bb614378904a73bb4d147e7236944c7198545
Hafizh24 <hafizhcbs@gmail.com>
2024-10-03 15:57:52 +0700

    feat: cart page

--- app/Helpers/CartManagement.php
+++ app/Helpers/CartManagement.php
@@ -82,12 +82,61 @@ class CartManagement
         Cookie::queue('cart_items', json_encode($cart_items), 60 * 24 * 7);
     }
 
+    static public function removeCartItem($product_id)
+    {
+        $cart_items = self::getCartItemsFromCookie();
+
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
+                unset($cart_items[$key]);
+            }
+        }
+
+        self::addCartItemsToCookie($cart_items);
+
+        return $cart_items;
+    }
+
     static public function getCartItemsFromCookie()
     {
         $cart_items = json_decode(Cookie::get('cart_items'), true);
         if (!$cart_items) {
             $cart_items = [];
         }
+
+        return $cart_items;
+    }
+
+    static public function incrementQuantityToCartItem($product_id)
+    {
+        $cart_items = self::getCartItemsFromCookie();
+
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
+                $cart_items[$key]['quantity']++;
+                $cart_items[$key]['total_amount'] = $cart_items[$key]['quantity'] * $cart_items[$key]['unit_amount'];
+            }
+        }
+
+        self::addCartItemsToCookie($cart_items);
+
+        return $cart_items;
+    }
+
+    static public function decrementQuantityToCartItem($product_id)
+    {
+        $cart_items = self::getCartItemsFromCookie();
+
+        foreach ($cart_items as $key => $item) {
+            if ($item['product_id'] == $product_id) {
+                if ($cart_items[$key]['quantity'] > 1) {
+                    $cart_items[$key]['quantity']--;
+                    $cart_items[$key]['total_amount'] = $cart_items[$key]['quantity'] * $cart_items[$key]['unit_amount'];
+                }
+            }
+        }
+
+        self::addCartItemsToCookie($cart_items);
 
         return $cart_items;
     }
